ea2ab4ba2aa2e099a1f04a280b6ae90215d0b1cc
Eugene <nralum@yandex.ru>
2021-05-21 18:54:33 +0400

    Переход обратно

--- server/main/urls.py
+++ server/main/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('index', views.index, name='home1'),
     path('treners', views.treners, name='treners'),
-    path('master_classes', views.treners, name='master_classes'),
+    path('master_classes', views.master_classes, name='master_classes'),
 
 ]
